c1b6c2ce51dac524ee371f6f85b67e72f5792b24
camillyalmeida03 <125749729+camillyalmeida03@users.noreply.github.com>
2024-09-12 08:55:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function abrirbarra(){
+        document.getElementById("botoes").style.display = "inline";
+        document.getElementById("vlibras").style.display = "none";
+        document.getElementById("vlibras2").style.display = "inline";
+        
+    }
+    
+    function fecharbarra(){
+        document.getElementById("botoes").style.display = "none";
+        document.getElementById("vlibras").style.display = "inline";
+        document.getElementById("vlibras2").style.display = "none";
+    }
